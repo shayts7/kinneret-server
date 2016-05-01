d01b6108139b23d8b50fd824384855b367468bf6
boriska70@gmail.com <boriska70@gmail.com>
2016-05-02 01:11:15 +0300

    try AWS CodeDeploy

--- setenvironment.sh
+++ setenvironment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
-java -version
+#sudo add-apt-repository -y ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install -y oracle-java8-installer
+#sudo apt-get install -y oracle-java8-set-default
+#java -version
 mkdir -p /home/ubuntu/kinneret-server/
 rm -rf /home/ubuntu/kinneret-server/*
