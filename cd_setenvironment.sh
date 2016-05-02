d58f16c618aa3352b9f9070e159bf0ecb1564acd
boriska70@gmail.com <boriska70@gmail.com>
2016-05-02 14:41:55 +0300

    try AWS CodeDeploy

--- cd_setenvironment.sh
+++ cd_setenvironment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#sudo add-apt-repository -y ppa:webupd8team/java
-#sudo apt-get update
-#sudo apt-get install -y oracle-java8-installer
-#sudo apt-get install -y oracle-java8-set-default
-#java -version
-mkdir -p /home/ubuntu/kinneret-server/
-rm -rf /home/ubuntu/kinneret-server/*
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
