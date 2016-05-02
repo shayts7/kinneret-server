5a4bc152a784cb2c02e49ed9b944be54965a9332
boriska70@gmail.com <boriska70@gmail.com>
2016-05-02 15:29:51 +0300

    try AWS CodeDeploy

--- cd_startapp.sh
+++ cd_startapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$(which java) -jar /home/ubuntu/server/kinneret-server-1.0-SNAPSHOT.jar server /home/ubuntu/server/kinneret-server-ubuntu.yml &
+nohup $(which java) -jar /home/ubuntu/server/kinneret-server-1.0-SNAPSHOT.jar server /home/ubuntu/server/kinneret-server-ubuntu.yml &
 sleep 10
