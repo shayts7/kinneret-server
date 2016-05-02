0ebba51961c9365857c1687121ad910136c57f55
boriska70@gmail.com <boriska70@gmail.com>
2016-05-02 15:37:08 +0300

    try AWS CodeDeploy

--- cd_setenvironment.sh
+++ cd_setenvironment.sh
@@ -5,3 +5,5 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
+
+ps -ef | grep kinneret-server | grep -v grep | tr -s ' ' | cut -d' ' -f2 | xargs kill -9
